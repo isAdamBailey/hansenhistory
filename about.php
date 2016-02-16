5fdf6dbcda377b72c2d94e39911c71b070766515
Adam Bailey <owlnofeathers@gmail.com>
2016-02-16 13:51:15 -0800

    Added carousel to about page

--- about.php
+++ about.php
@@ -14,10 +14,66 @@
 
     <div class="row">
       <div class="col-md-12">
-        <figure class="center-block obit">
+        <!-- <figure class="center-block obit">
         	<img class="img-rounded img-responsive" src="images/Walter Hansen-28 roadster.jpg" alt="Walter Hansen and his 1928 roadster">
         	<figcaption class="text-center">Walter E Hansen and his 1928 roadster</figcaption>
-        </figure><br>
+        </figure><br> -->
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+		  </ol>
+
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" role="listbox">
+		    <div class="item active">
+		      <img src="images/Walter Hansen and his 28 roadster.jpg" alt="Walter E. Hansen and his 1928 roadster">
+		      <div class="carousel-caption">
+		        <h4>Walter E. Hansen and his 1928 roadster</h4>
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="images/Behind Barn.jpg" alt="View from behind the barn">
+		      <div class="carousel-caption">
+		        <h4>View from behind the barn</h4>
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="images/Driveway.jpg" alt="View from 12th Ave.">
+		      <div class="carousel-caption">
+		        <h4>View from 12th Ave.</h4>
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="images/Ranch Sign.jpg" alt="Hansen North Fork Ranch">
+		      <div class="carousel-caption">
+		        <h4>North Fork Ranch, Hansen Family Farm</h4>
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="images/Barn.jpg" alt="Front of barn">
+		      <div class="carousel-caption">
+		        <h4>The Barn</h4>
+		      </div>
+		    </div>
+		  </div>
+
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+
+
         <div class="panel panel-default">
           <div class="panel-heading">
             <h3 class="panel-title">The Property</h3>

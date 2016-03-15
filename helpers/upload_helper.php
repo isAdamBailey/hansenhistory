1bbe39d93280e927ba38d65b4d59884b555b5b90
Adam Bailey <owlnofeathers@gmail.com>
2016-03-15 11:12:36 -0700

    Moved obituary image upload code to helper function

--- helpers/upload_helper.php
+++ helpers/upload_helper.php
@@ -52,3 +52,53 @@ function uploadImage()
   	}
 
 }
+
+function uploadObitImage()
+{
+	global $db, $ob;
+
+	if(isset($_POST['submit'])){
+	    $target_dir = '../images/obits/';
+	    $target_file = $target_dir . basename($_FILES['image']['name']);
+	    $uploadOk = 1;
+	    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	    $imagepath = basename($_FILES['image']['name']);
+
+	    $name = mysqli_real_escape_string($db->link, $_POST['name']);
+	    $obituary = mysqli_real_escape_string($db->link, $_POST['obituary']);
+	    $birthdate = mysqli_real_escape_string($db->link, $_POST['dob']);
+	    $deathdate = mysqli_real_escape_string($db->link, $_POST['dod']);
+
+	    // simple validation
+	    if($name == ''|| $obituary == ''|| $birthdate ==''|| $deathdate == ''){
+	      // set error
+	      $error = 'Please fill out all required fields.';
+	    }
+
+	    // image upload validation
+	    $check = getimagesize($_FILES["image"]["tmp_name"]);
+	    if($check !== false) {
+	        $error = 'File is an image - ' . $check["mime"] . '.';
+	        $uploadOk = 1;
+	    } else {
+	        $error = 'File is not an image.';
+	        $uploadOk = 0;
+	    } 
+	    // Allow certain file formats
+	    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	    && $imageFileType != "gif" ) {
+	        $error = 'Sorry, only JPG, JPEG, PNG & GIF files are allowed.';
+	        $uploadOk = 0;
+	    }
+	    // Check if $uploadOk is set to 0 by an error
+	     if ($uploadOk == 0) {
+	         $error = 'Sorry, your file was not uploaded.';
+	    } else {
+	     
+	      // upload the file
+	      move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+
+	      $insert_row = $db->insert($ob->setObituary($imagepath, $name, $obituary, $birthdate, $deathdate));
+	    }
+	  }
+}
